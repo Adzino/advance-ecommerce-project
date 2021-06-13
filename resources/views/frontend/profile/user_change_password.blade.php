7eea1e90212be0d6029993e941b8d0947adefb4c
Adzino <adzikin@gmail.com>
2021-06-13 21:21:33 +0200

    Multi Auth with theme

---
+++ resources/views/frontend/profile/user_change_password.blade.php
@@ -0,0 +1,63 @@
+@extends('frontend.main_master')
+@section('content')
+
+<!-- Nahrada za Eloquent method v IndexController v UserChangePassword() -->
+{{-- @php
+    $user = DB::table('users')->where('id', Auth::user()->id)->first();
+@endphp --}}
+
+<div class="body-content">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-2"><br><br>
+                <img class="card-img-top" style="border-radius: 50%" src="{{  (!empty($user->profile_photo_path))? url('upload/user_images/'.$user->profile_photo_path):url('upload/no_image.jpg') }}" height="100%" width="100%"><br><br>
+                <ul class="list-group list-group-flush">
+                    <a href="{{ route('dashboard') }}" class="btn btn-primary btn-sm btn-block">Home</a>
+                    <a href="{{ route('user.profile') }}" class="btn btn-primary btn-sm btn-block">Profile Upate</a>
+                    <a href="" class="btn btn-primary btn-sm btn-block">Change password</a>
+                    <a href="{{ route('user.logout') }}" class="btn btn-danger btn-sm btn-block">Logout</a>
+                </ul>
+            </div> <!-- end col md 2 -->
+
+            <div class="col-md-2">
+
+            </div> <!-- end col md 2 -->
+
+            <div class="col-md-6">
+                <div class="card">
+                    <h3 class="text-center"><span class="text-danger"> Change Password </span></h3>
+                
+                        <div class="cart-body">
+                            <form method="post" action="{{ route('user.password.update') }}">
+                                @csrf
+                                
+                                <div class="form-group">
+                                    <label class="info-title" for="exampleInputEmail1">Current Password </label>
+                                    <input type="password" id="current_password" name="oldpassword" class="form-control">
+                                </div>
+
+                                <div class="form-group">
+                                    <label class="info-title" for="exampleInputEmail1">New Password </label>
+                                    <input type="password" id="password" name="password" class="form-control">
+                                </div>
+
+                                <div class="form-group">
+                                    <label class="info-title" for="exampleInputEmail1">Confirm Password </label>
+                                    <input type="password" id="password_confirmation" name="password_confirmation" class="form-control">
+                                </div>
+
+                                <div class="form-group">
+                                    <button type="submit" class="btn btn-danger">Update</button>
+                                </div>
+
+                            </form>
+                        </div>
+                
+                </div>
+            </div> <!-- end col md 6 -->
+
+        </div> <!-- end row -->
+    </div>
+</div>
+
+@endsection
